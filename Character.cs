d26d49a59cec65ddf030e3c8e16f84d4279cb037
done778 <57587831+done778@users.noreply.github.com>
2025-10-01 15:42:48 +0900

    [feat] Add character abstract class
    캐릭터 추상 클래스와 그걸 상속받는 적 클래스 추가. 적은 기본적인 능력 외 추가 능력은 없다.

---
+++ Character.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RaidStrategy
+{
+    // 내 캐릭터 및 적들이 모두 공통적으로 가지는 필드와 메서드
+    abstract class Character
+    {
+        protected string name;
+        protected int statusAttack;
+        protected int statusHealth;
+        protected bool isAlive;
+
+        public Character() 
+        {
+            isAlive = true;
+        }
+
+        public void TakeDamage(int _damage)
+        {
+            statusHealth -= _damage;
+        }
+
+        public void Attack(Character _target)
+        {
+            _target.TakeDamage(statusAttack);
+        }
+        public void Death()
+        {
+            isAlive = false;
+        }
+    }
+
+    // 적은 이름과 공격력, 체력 스탯만 생성자로 초기화하고 공격 외 추가 능력은 없다.
+    class Enemy : Character 
+    {
+        public Enemy(string _name, int _att, int _hp) 
+        {
+            name = _name;
+            statusAttack = _att;
+            statusHealth = _hp;
+        }
+    }
+}
